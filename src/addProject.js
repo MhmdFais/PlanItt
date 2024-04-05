e679ba9c8cb6d21828759662eb4557d02e1e854d
404CoderM <117585798+404CoderM@users.noreply.github.com>
2024-04-05 22:05:15 +0530

    Refactor AddProject class

--- src/addProject.js
+++ src/addProject.js
@@ -1,41 +1,53 @@
-export class AddProject{
-    constructor(){
-        this.dialog = document.querySelector('#project-dialog'); 
-        this.projectNameInput = document.querySelector('#proj-name');
-        this.saveButton = document.querySelector('#input-close-save'); 
-        this.closeButton = document.querySelector('#input-close-cancel');
+export class AddProject {
+  constructor() {
+    this.dialog = document.querySelector("#project-dialog");
+    this.projectNameInput = document.querySelector("#proj-name");
+    this.saveButton = document.querySelector("#input-close-save");
+    this.closeButton = document.querySelector("#input-close-cancel");
 
-        this.saveButton.addEventListener('click', this.saveProject.bind(this));
-        this.closeButton.addEventListener('click', this.closeDialog.bind(this));
+    this.saveButton.addEventListener("click", this.saveProject.bind(this));
+    this.closeButton.addEventListener("click", this.closeDialog.bind(this));
 
-        this.render();
+    this.render();
+  }
+
+  render() {
+    this.dialog.showModal();
+  }
+
+  saveProject(event) {
+    event.preventDefault();
+    if (this.projectNameInput.value.trim() === "") {
+      this.alertLog("Please enter a project name");
+    } else {
+      this.addToProjects();
     }
+  }
 
-    render() {
-        this.dialog.showModal();
-    }
+  closeDialog(event) {
+    event.preventDefault();
+    this.projectNameInput.value = "";
+    this.dialog.close();
+  }
 
-    saveProject(event) {
-        event.preventDefault(); 
-        if (this.projectNameInput.value.trim() === '') {
-            this.alertLog("Please enter a project name");
-        } else {
-            this.addToProjects();
-        }
-    }
+  alertLog(message) {
+    console.log(message);
+  }
 
-    closeDialog(event) {
-        event.preventDefault(); 
-        this.projectNameInput.value = '';
-        this.dialog.close();
+  addToProjects() {
+    if (this.projectNameInput.value.trim() === "") {
+      console.log("No name");
+    } else {
+      console.log("Project saved:", this.projectNameInput.value);
+      this.unorderedList = document.querySelector(".nav-list-projects");
+      this.listElement = document.createElement("li");
+      this.listElement.appendChild(
+        document.createTextNode(this.projectNameInput.value)
+      );
+      this.listElement.classList.add(this.projectNameInput.value);
+      this.unorderedList.appendChild(this.listElement);
+      this.projectNameInput.value = "";
+      this.dialog.close();
     }
-
-    alertLog(message){
-        console.log(message);
-    }
-
-    addToProjects(){
-        console.log('Project saved:', this.projectNameInput.value);
-        this.dialog.close();
-    }
+  }
 }
